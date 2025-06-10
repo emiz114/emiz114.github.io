9f5331c261f2732c087c90bdd23a8a9f45488df3
emiz114 <emiz@seas.upenn.edu>
2025-06-10 14:39:44 -0400

    updated

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function sayHi() {
-  alert("Hi from your website!");
-}
+const text = "Senior at Penn Engineering";
+  const element = document.getElementById("typewriter");
+
+  let i = 0;
+  function typeChar() {
+    if (i < text.length) {
+      const currentChar = text[i] === '\n' ? '<br>' : text[i];
+      element.innerHTML += currentChar;
+      i++;
+      setTimeout(typeChar, 100); // speed in ms
+    }
+  }
+
+  typeChar();
